ed7b13d4e3dac6fce58e19e6043ed686a347dedf
Darius <darius@dubiza.io>
2024-12-05 10:46:49 -0700

    Add all_projects route to urls.py and setup view

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
-def project_list(request):
-    return render(request, 'projects/index.html')
+def all_projects(request):
+    # query the db to return all project objects
+    projects = Project.objects.all()
+    return render(request, 'all_projects.html', {'projects': projects})
