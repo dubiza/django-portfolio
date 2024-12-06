41e712263284cfe4452e16ca86ff428e9f7a710d
Darius <darius@dubiza.io>
2024-12-06 09:47:22 -0700

    Update view to render database data

--- projects/views.py
+++ projects/views.py
@@ -5,4 +5,4 @@ from projects.models import Project
 def all_projects(request):
     # query the db to return all project objects
     projects = Project.objects.all()
-    return render(request, 'all_projects.html', {'projects': projects})
+    return render(request, 'projects/all_projects.html', {'projects': projects})
